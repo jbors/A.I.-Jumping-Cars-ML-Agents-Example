935650912ffd6e9b7a1a99bd667d86abc483ac96
Joris Borsboom <jorisborsboom@gmail.com>
2023-11-03 07:33:57 +0100

    Solution 2022

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 using UnityEngine;
 
-public class Jumper : MonoBehaviour
+public class Jumper : Agent
 {
     [SerializeField] private float jumpForce;
     [SerializeField] private KeyCode jumpKey;
@@ -16,11 +17,6 @@ public class Jumper : MonoBehaviour
     private int score = 0;
     public event Action OnReset;
     
-    public void Awake()
-    {
-        rBody = GetComponent<Rigidbody>();
-        startingPosition = transform.position;
-    }
     
     private void Jump()
     {
@@ -29,12 +25,6 @@ public class Jumper : MonoBehaviour
             rBody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
             jumpIsReady = false;
         }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKey(jumpKey))
-            Jump();
     }
 
     private void Reset()
@@ -51,19 +41,62 @@ public class Jumper : MonoBehaviour
 
     private void OnCollisionEnter(Collision collidedObj)
     {
-        if (collidedObj.gameObject.CompareTag("Street"))
-            jumpIsReady = true;
         
-        else if (collidedObj.gameObject.CompareTag("Mover") || collidedObj.gameObject.CompareTag("DoubleMover"))
+        if (collidedObj.gameObject.CompareTag("Street")){
+            jumpIsReady = true;
+        }
+        
+        else if (collidedObj.gameObject.CompareTag("Mover") || collidedObj.gameObject.CompareTag("DoubleMover")){
+            AddReward(-1.0f);
             Reset();
+            EndEpisode();
+        }
     }
 
     private void OnTriggerEnter(Collider collidedObj)
     {
         if (collidedObj.gameObject.CompareTag("score"))
         {
+            AddReward(0.05f);
             score++;
             ScoreCollector.Instance.AddScore(score);
+            if(score > 100){
+                AddReward(1f);
+                EndEpisode();
+            }
+        }
+    }
+
+    public override void Initialize(){
+        rBody = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+    }
+
+    public override void OnActionReceived(ActionBuffers actions){
+        var Actions = actions.DiscreteActions;
+        if (Mathf.FloorToInt(Actions[0]) == 1){
+            Jump();  
+        }  
+    }
+
+    public override void OnEpisodeBegin(){
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut){
+        var Actions = actionsOut.DiscreteActions;
+
+        Actions[0] = 0;
+
+        if (Input.GetKey(jumpKey)){
+            Actions[0] = 1;  
+        }
+    }
+
+    private void FixedUpdate()  
+    {  
+        if(jumpIsReady){     
+            RequestDecision();  
         }
     }
 }
